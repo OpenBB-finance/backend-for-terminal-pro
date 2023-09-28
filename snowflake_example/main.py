e897d0ce352aba900ed0c284f43104d4746f6e26
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-09-28 01:43:11 -0700

    add cybersin example

--- snowflake_example/main.py
+++ snowflake_example/main.py
@@ -10,13 +10,15 @@ import datetime
 
 ## Account can be found in Accounts under Admin - ex. https://xxxxxx-xxxxxx.snowflakecomputing.com
 
+# Data used is from: https://app.snowflake.com/marketplace/listing/GZTSZAS2KIM/cybersyn-inc-cybersyn-weather-environmental-essentials?search=cybersin&sortBy=relevant&pricing=free
+
 conn = snowflake.connector.connect(
     user="",
     password="",
     account="",
     warehouse="COMPUTE_WH",
-    database="ECONOMY_DATA_ATLAS",
-    schema="ECONOMY",
+    database="Cybersyn_Weather__Environmental_Essentials",
+    schema="CYBERSIN",
 )
 
 
@@ -64,19 +66,28 @@ def get_widgets():
     return JSONResponse(content=data)
 
 
-@app.get("/snowflake")
-def get_snowflake_data():
+@app.get("/cybersyn_severe_weather_days_in_florida")
+def get_cybersyn_severe_weather_days_in_florida():
     """Return snowflake data"""
 
     try:
         cur = conn.cursor()
-        cur.execute("select * from DATA_ATLAS limit 10")
-        query_id = cur.sfqid
-        while conn.is_still_running(conn.get_query_status_throw_if_error(query_id)):
-            time.sleep(1)
-
+        cur.execute("""
+SELECT
+    YEAR(ts.date) AS year,
+    COUNT(DISTINCT ts.date) AS count_severe_weather_days
+FROM cybersyn.noaa_weather_metrics_timeseries AS ts
+JOIN cybersyn.noaa_weather_station_index AS idx
+    ON (ts.noaa_weather_station_id = idx.noaa_weather_station_id)
+WHERE 
+    ts.variable_name = 'Weather Type: Tornado, Waterspout, or Funnel Cloud'
+    AND idx.state_name = 'Florida'
+    AND ts.value = 1
+    AND ts.date >= '2010-01-01'
+GROUP BY year
+ORDER BY year;
+        """)
         results = cur.fetchall()
-        print(f"{results}")
     except ProgrammingError as err:
         print("Programming Error: {0}".format(err))
 
