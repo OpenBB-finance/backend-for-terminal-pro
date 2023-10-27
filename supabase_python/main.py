5a07683fa06abbd9834e8ad872ed630a07741301
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-10-27 01:44:59 -0700

    improve supabase

--- supabase_python/main.py
+++ supabase_python/main.py
@@ -1,19 +1,15 @@
-import os
 import json
-import requests
-import time
-import json
-
-from supabase import create_client, Client
-
-url: str = ""
-key: str = ""
-supabase: Client = create_client(url, key)
 
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+from supabase import create_client, Client
+
+supabase: Client = create_client(
+    url="",
+    key=""
+)
 
 app = FastAPI()
 
@@ -35,14 +31,14 @@ app.add_middleware(
 ## Endpoints
 @app.get("/")
 def read_root():
-    return {"Info": "Backend Template for the OpenBB Terminal Pro"}
+    return {"Info": "Supabase backend template for the OpenBB Terminal Pro"}
 
 
 @app.get("/widgets.json")
 def get_widgets():
     """Widgets configuration file for the OpenBB Terminal Pro"""
     file_path = "widgets.json"
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding='utf-8') as file:
         data = json.load(file)
     return JSONResponse(content=data)
 
