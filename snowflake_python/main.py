dd09a3e9716f04ddc7778baa1dd3ed82b1a6c31c
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-10-27 01:50:44 -0700

    update snowflake

--- snowflake_python/main.py
+++ snowflake_python/main.py
@@ -1,16 +1,13 @@
-import os
 import json
-import requests
-import time
 
-##python snowflake connector https://docs.snowflake.com/en/developer-guide/python-connector/python-connector
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 from snowflake.connector import ProgrammingError
 import snowflake.connector
-import datetime
 
-## Account can be found in Accounts under Admin - ex. https://xxxxxx-xxxxxx.snowflakecomputing.com
-
-# Data used is from: https://app.snowflake.com/marketplace/listing/GZTSZAS2KIM/cybersyn-inc-cybersyn-weather-environmental-essentials?search=cybersin&sortBy=relevant&pricing=free
+# Data from: https://app.snowflake.com/marketplace/listing/GZTSZAS2KIM/cybersyn-inc-cybersyn-weather-environmental-essentials?search=cybersin&sortBy=relevant&pricing=free
 
 conn = snowflake.connector.connect(
     user="",
@@ -21,18 +18,11 @@ conn = snowflake.connector.connect(
     schema="CYBERSIN",
 )
 
-
 # Define a function to convert Snowflake ResultSet to a JSON-friendly format
 def snowflake_to_json(result_set, cur):
     columns = [col[0] for col in cur.description]
     results_list = [dict(zip(columns, row)) for row in result_set]
     return results_list
-
-
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
-
 
 app = FastAPI()
 
@@ -53,14 +43,14 @@ app.add_middleware(
 ## Endpoints
 @app.get("/")
 def read_root():
-    return {"Info": "Backend Template for the OpenBB Terminal Pro"}
+    return {"Info": "Snowflake backend template for the OpenBB Terminal Pro"}
 
 
 @app.get("/widgets.json")
 def get_widgets():
     """Widgets configuration file for the OpenBB Terminal Pro"""
     file_path = "widgets.json"
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding='utf-8') as file:
         data = json.load(file)
     return JSONResponse(content=data)
 
