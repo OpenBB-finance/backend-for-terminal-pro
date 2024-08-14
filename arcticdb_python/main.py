8660b6d2c31db455a0db4d73ce66e8170efe9e08
andrewkenreich <andrew.kenreich@gmail.com>
2024-08-13 22:48:03 -0400

    3 widgets

--- arcticdb_python/main.py
+++ arcticdb_python/main.py
@@ -9,7 +9,7 @@ import time
 import numpy as np
 import pandas as pd
 from datetime import datetime
-# import arcticdb as adb
+import arcticdb as adb
 
 app = FastAPI()
 
@@ -30,10 +30,10 @@ app.add_middleware(
 )
 
 # # For this demo we will configure the LMDB file based backend. ArcticDB achieves its high performance and scale when configured with an object store backend (e.g. S3).
-# arctic = adb.Arctic("lmdb://arcticdb_demo")
+arctic = adb.Arctic("lmdb://arcticdb_demo")
 
 # # You can have an unlimited number of libraries, but we will just create one to start with.
-# lib = arctic.get_library('sample', create_if_missing=True)
+lib = arctic.get_library('sample', create_if_missing=True)
 
 # n = 10_000
 # large = pd.DataFrame(
@@ -60,29 +60,38 @@ def get_widgets():
 @app.get("/large_dataframe")
 def large_dataframe():
 
-    # Generate random data for the specified columns
-    data = {
-        'c0': np.random.rand(5),
-        'c1': np.random.rand(5),
-        'c5000': np.random.rand(5),
-        'c5001': np.random.rand(5),
-        'c9998': np.random.rand(5),
-        'c9999': np.random.rand(5)
-    }
+    # Read the subframe from the random dataframe
+    subframe = lib.read(
+        "large",
+        columns=["c0", "c1", "c5000", "c5001", "c9998", "c9999"],
+        date_range=(datetime(2020, 6, 13, 8), datetime(2020, 6, 13, 13))
+    ).data
 
-    # Create datetime index
-    date_range = pd.date_range(start='2020-06-13 08:00:00', periods=5, freq='H')
+    # convert df to json
+    return subframe.to_dict(orient="records")
 
-    # Create the dataframe
-    subframe = pd.DataFrame(data, index=date_range)
-    subframe = subframe.reset_index().rename(columns={'index': 'date'})
+@app.get("/large_dataframe2")
+def large_dataframe2():
 
     # Read the subframe from the random dataframe
-    # subframe = lib.read(
-    #     "large",
-    #     columns=["c0", "c1", "c5000", "c5001", "c9998", "c9999"],
-    #     date_range=(datetime(2020, 6, 13, 8), datetime(2020, 6, 13, 13))
-    # ).data
+    subframe = lib.read(
+        "large",
+        columns=["c7", "c6", "c6000", "c4001", "c9398", "c9999"],
+        date_range=(datetime(2020, 5, 13, 8), datetime(2020, 5, 13, 13))
+    ).data
+
+    # convert df to json
+    return subframe.to_dict(orient="records")
+
+@app.get("/large_dataframe3")
+def large_dataframe3():
+
+    # Read the subframe from the random dataframe
+    subframe = lib.read(
+        "large",
+        columns=["c3", "c2", "c5000", "c4001", "c9998", "c4999"],
+        date_range=(datetime(2020, 4, 13, 8), datetime(2020, 4, 13, 13))
+    ).data
 
     # convert df to json
     return subframe.to_dict(orient="records")
