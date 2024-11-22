2c77b6cc380c96a8ddc9162e45cfb30d79734b24
James Maslek <jmaslek11@gmail.com>
2024-11-22 15:49:15 -0500

    Add snowflake backend example

---
+++ snowflake_connector_python/utils.py
@@ -0,0 +1,17 @@
+import os
+
+import snowflake.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_snowflake_connection():
+    return snowflake.connector.connect(
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        role=os.getenv("SNOWFLAKE_ROLE"),
+        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+        database=os.getenv("SNOWFLAKE_DATABASE"),
+    )
