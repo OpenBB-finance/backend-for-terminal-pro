ece0c1c2a78ab1345a3f477cf709a75106c40ddd
andrewkenreich <andrew.kenreich@gmail.com>
2024-12-16 14:07:31 -0500

    template update

--- advanced_examples/custom_templates/main.py
+++ advanced_examples/custom_templates/main.py
@@ -1,8 +1,9 @@
 import json
 from pathlib import Path
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+import requests
 
 app = FastAPI()
 
@@ -34,16 +35,43 @@ def get_widgets():
         content=json.load((Path(__file__).parent.resolve() / "widgets.json").open())
     )
 
-@app.get("/json-data")
-def json_data():
-    """Read mock csv data and return it as a table to your widget"""
-    # Specify the path to your JSON file
-    json_file_path = "mock_data.json"
+@app.get("/chains_table")
+def chains_table(chain: str = Query(None, description="Chain to filter by")):
+    """Get current TVL of all chains using Defi LLama"""
+    params = {}
+    response = requests.get("https://api.llama.fi/v2/chains", params=params)
 
-    try:
-        # Return the JSON data as is
-        return json.load((ROOT_PATH / json_file_path).open()).get("stocks", [])
-    except Exception as e:
-        # Handle error cases here
-        error_message = f"Error reading the JSON file: {str(e)}"
-        return JSONResponse(content={"error": error_message}, status_code=500)
+    if response.status_code == 200:
+        data = response.json()
+        # Filter by the chain parameter if provided
+        if chain:
+            data = [entry for entry in data if entry.get("name").lower() == chain.lower()]
+        
+        return data
+    
+
+
+    print(f"Request error {response.status_code}: {response.text}")
+    return JSONResponse(
+        content={"error": response.text}, status_code=response.status_code
+    )
+
+# example of how to get a dropdown list of chains
+@app.get("/get_chains_list")
+def get_chains_list():
+    """Get list of chains using Defi LLama"""
+    response = requests.get("https://api.llama.fi/v2/chains")
+
+    if response.status_code == 200:
+        data = response.json()
+        # can pass as list of {label, value} for dropdown or list of strings
+        #  [
+        #   {"label": chain.get("name"), "value": chain.get("name")}
+        #   for chain in data if chain.get("name")
+        #  ]
+        return [chain.get("name") for chain in data if chain.get("name")]
+
+    print(f"Request error {response.status_code}: {response.text}")
+    return JSONResponse(
+        content={"error": response.text}, status_code=response.status_code
+    )
